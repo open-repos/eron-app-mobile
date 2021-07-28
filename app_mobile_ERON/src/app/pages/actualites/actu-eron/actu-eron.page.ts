730aa4e57bc43477b9e5ba17ae9a884dc340ff57
acapai <andria.c@eronservice.fr>
2021-07-28 12:48:40 +0200

    *Ttitre commit: Actualité Eron service et mise en page *Changement(s) effectué(s): - Ajout de slides pour visualiser les actu - Ajout de ngForm et AlertController pour abonnement newsletter *TODO: - Ajouter page et navigation de la slide actualité vers le détail actualité (page détail actualité à créer) [ticket: X](si BUG)

--- app_mobile_ERON/src/app/pages/actualites/actu-eron/actu-eron.page.ts
+++ app_mobile_ERON/src/app/pages/actualites/actu-eron/actu-eron.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ActutalitesEron } from './actu-eron.model';
+import { ActualitesEronService } from './../../../services/actualites-eron.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, IonSlides } from '@ionic/angular';
+import { Formation } from '../../formations/formation.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-actu-eron',
@@ -7,9 +13,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActuEronPage implements OnInit {
 
-  constructor() { }
+  actualitesEron: ActutalitesEron[];
+  @ViewChild(IonSlides) slider: IonSlides;
+  
+  constructor(private router: Router, private actualitesService: ActualitesEronService,
+    private alertCtrl: AlertController
+    ) {
 
-  ngOnInit() {
   }
+  
+  ngOnInit() {
+    this.actualitesEron = this.actualitesService.getAllactualiteEron();
+  }
+
+  onSubmit(form: NgForm) {
+    const email = form.value['email']
+
+    const validEmail = this.validateEmail(email)
+    if (form.invalid || !validEmail){
+      this.alertCtrl
+      .create({
+        header: 'Erreur',
+        message: 'Veuillez rentrez une adresse email valide',
+        buttons: ['OK']
+      })
+      .then(alertEl => {
+        alertEl.present();
+      }); 
+    } else {
+    this.alertCtrl
+    .create({
+      header: 'Felicitations',
+      message: 'Vous êtes maintenant abonné a la newsletter ERON',
+      buttons: ['OK']
+    })
+    .then(alertEl => {
+      alertEl.present();
+    });
+  }
+    // this.appareilService.addAppareil(email);
+}
+
+validateEmail(email: string) {
+  const regularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regularExpression.test(String(email).toLowerCase());
+
+ }
 
 }
