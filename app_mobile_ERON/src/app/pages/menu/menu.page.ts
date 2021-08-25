6753de3aab6c87ae7a7ee757158b0ee378029a7f
acapai <andria.c@eronservice.fr>
2021-08-25 13:14:54 +0200

    *Titre commit: Social Media App Connection *Changement(s) effectué(s): - Installation de package : appavailability et inappbrowser - creation des fonctions pour ouvrir soit une app soit le browser pour l'application souhaité - creation d'une fonction avec switch case pour renvoyer vers les fonctions d'ouvertures de média sociaux

--- app_mobile_ERON/src/app/pages/menu/menu.page.ts
+++ app_mobile_ERON/src/app/pages/menu/menu.page.ts
@@ -1,4 +1,6 @@
 import { MenuItem } from './menu.model';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { AppAvailability } from '@ionic-native/app-availability/ngx';
 import { Component, Input, OnInit } from '@angular/core';
 import {
   faFacebookSquare,
@@ -26,18 +28,21 @@ import {
   faQuestionCircle as farQuestionCircle,
 } from '@fortawesome/free-regular-svg-icons';
 import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
+  providers: [InAppBrowser, AppAvailability],
 })
 export class MenuPage implements OnInit {
   
   socialMedia = [
-    {icon:faFacebookSquare},
-    {icon:faTwitterSquare},
-    {icon:faInstagramSquare},
-    {icon:faLinkedin},
+    {icon:faFacebookSquare,app:'FACEBOOK'},
+    {icon:faTwitterSquare,app:'TWITTER'},
+    {icon:faInstagramSquare,app:'INSTAGRAM'},
+    {icon:faLinkedin,app:'LINKEDIN'},
   ]
 
   // @Input() formationItem: string;
@@ -138,8 +143,13 @@ export class MenuPage implements OnInit {
     },
   ];
 
+app: string;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private platform: Platform,
+    private inAppBrowser: InAppBrowser,
+    private appAvailability: AppAvailability
+    ) {}
 
   ngOnInit() {
     // this.menu.enable(true,'menu-visitor')
@@ -150,4 +160,109 @@ export class MenuPage implements OnInit {
     // this.router.navigateByUrl(url, {state:{example:value}})
     this.router.navigate([url],  { queryParams: { formation: value } })
   }
+
+  socialMediaFct(type: string) {
+    switch (type) {
+      case 'FACEBOOK': {
+        this.openFacebook('ERON.FormationSante', 'https://www.facebook.com/ERON.FormationSante/');
+        break;
+      }
+      case 'INSTAGRAM': {
+        this.openInstagram('eron_sante')
+        break;
+      }
+      case 'TWITTER': {
+        this.openTwitter('eron_sante');
+        break;
+      }
+      case 'LINKEDIN':{
+        this.openLinkedin('eron-sante');
+        break;
+      }
+    }
+  }
+
+
+  openTwitter(name: string) {
+    // let app;
+    if (this.platform.is('ios')) {
+      this.app = 'twitter://';
+    } else if (this.platform.is('android')) {
+      this.app = 'com.twitter.android';
+    } else {
+      this.openInApp('https://twitter.com/' + name);
+      return;
+    }
+    this.appAvailability.check(this.app)
+      .then((res) => {
+        const data = 'twitter://user?screen_name=' + name;
+        this.openInApp(data);
+      }
+      ).catch(err => {
+        this.openInApp('https://twitter.com/' + name);
+});
+  }
+  openFacebook(name: string, url:string) {
+    // let app : string;
+    if (this.platform.is('ios')) {
+      this.app = 'fb://';
+    } else if (this.platform.is('android')) {
+      this.app = 'com.facebook.katana';
+    } else {
+      this.openInApp('https://www.facebook.com/' + name);
+      return;
+    }
+    this.appAvailability.check(this.app)
+      .then(res => {
+        const fbUrl = 'fb://facewebmodal/f?href=' + url;
+        this.openInApp(fbUrl);
+      }
+      ).catch(() => {
+        this.openInApp('https://www.facebook.com/' + name);
+      });
+  }
+
+  openInstagram(name: string) {
+    // let app;
+    if (this.platform.is('ios')) {
+      this.app = 'instagram://';
+    } else if (this.platform.is('android')) {
+      this.app = 'com.instagram.android';
+    } else {
+      this.openInApp('https://www.instagram.com/' + name);
+      return;
+    }
+    this.appAvailability.check(this.app)
+      .then((res) => {
+        this.openInApp('instagram://user?username=' + name);
+      }
+      ).catch(err => {
+        this.openInApp('https://www.instagram.com/' + name);
+      });
+  }
+
+
+  openLinkedin(name: string) {
+    // let app;
+    if (this.platform.is('ios')) {
+      this.app = 'linkedin://';
+    } else if (this.platform.is('android')) {
+      this.app = 'com.linkedin.android';
+    } else {
+      this.openInApp('https://www.linkedin.com/company/' + name);
+      return;
+    }
+    this.appAvailability.check(this.app)
+      .then((res) => {
+        this.openInApp('linkedin://user?username=' + name);
+      }
+      ).catch(err => {
+        this.openInApp('https://www.linkedin.com/company/' + name);
+      });
+  }
+
+  openInApp(url:string) {
+    this.inAppBrowser.create(url, '_system')
+  }
+
 }
