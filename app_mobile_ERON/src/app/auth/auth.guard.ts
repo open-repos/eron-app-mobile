8c8f8445909d6734663dc78727e40f9310cd0829
acapai <andria.c@eronservice.fr>
2022-02-08 19:32:50 +0100

    *Titre commit: Test d'une autre façon d'authentifier *Changement(s) effectué(s): - Condition boolean dans le constructeur du auth.service

--- app_mobile_ERON/src/app/auth/auth.guard.ts
+++ app_mobile_ERON/src/app/auth/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanLoad {
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const isAuthenticated = !!(+localStorage.getItem('authenticated'))
     console.log("is Auth:",this.authService.userIsAuthenticated) 
-    if(isAuthenticated){
+    if(this.authService.userIsAuthenticated){
       return true;
     }else{
       const navigation = this.router.getCurrentNavigation();
--- app_mobile_ERON/src/app/services/auth.service.ts
+++ app_mobile_ERON/src/app/services/auth.service.ts
@@ -7,13 +7,18 @@ export class AuthService {
 
   // Variable déclaré en "private" pour empecher accidentellement de transformer la variable en vrai ou faux ailleurs que dans les fonctions login et logout de ce service
   private _userIsAuthenticated: boolean;
-// TODO: remettre en false une fois développement terminé
+
   get userIsAuthenticated(){
     return this._userIsAuthenticated;
   }
 
   constructor() {
-   this._userIsAuthenticated=false;
+    if(!!(+localStorage.getItem('authenticated'))){
+      this._userIsAuthenticated=true;
+    } else {
+      this._userIsAuthenticated=false;
+    }
+
    }
 
   login(user:string){
